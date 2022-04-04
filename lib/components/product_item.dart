34d41929e685709a030d37201c48b4002fd02e9b
Wanderson Bleiner <wanderson.bleiner@gmail.com>
2022-04-04 16:39:20 -0300

    projeto shop Cod3r

---
+++ lib/components/product_item.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
+
+import '../utils/app_routes.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      title: Text(product.name),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.PRODUCT_FORM,
+                  arguments: product,
+                );
+              },
+              icon: const Icon(Icons.edit),
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Tem certeza?'),
+                    content: const Text('Quer remover esse item?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                        child: const Text('Sim'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(ctx).pop(false),
+                        child: const Text('Não'),
+                      ),
+                    ],
+                  ),
+                ).then(
+                  (value) => {
+                    if (value ?? false)
+                      {
+                        Provider.of<ProductList>(
+                          context,
+                          listen: false,
+                        ).deleteProduct(product.id)
+                      },
+                  },
+                );
+              },
+              icon: const Icon(Icons.delete),
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
